a3ce0562273dfa355f7813436e417aa55f87714d
MFMvanderholst <117632371+MFMvanderholst@users.noreply.github.com>
2023-05-30 11:38:39 +0200

    homepage is er

--- app/controllers/Rijexamen.php
+++ app/controllers/Rijexamen.php
@@ -2,5 +2,15 @@
 
     class Rijexamen extends BaseController
     {
-        
+        private $rijexamenmodel;
+
+        public function __construct()
+        {
+            $this->rijexamenmodel = $this->model('RijexamenModel');
+        }
+
+        public function OverzichtRijExamen()
+        {
+            
+        }
     }
--- app/views/Homepage/index.php
+++ app/views/Homepage/index.php
@@ -1,3 +1,3 @@
 <h3><?= $data['title']; ?></h3>
 <a href="http://www.mvc-2209a-framework-periode4.com/Instructeur/overzichtinstructeur">Instructeuren in dienst</a>
-<a href="http://www.be-p4-toets-336159/Rijexamen/index">Overzichtexamens</a>
+<a href="http://www.be-p4-toets-336159/Rijexamen/Overzichtexamens">Overzichtexamens</a>
