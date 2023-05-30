83e50451b1e574d321919fe6ba44fb2370f73c6c
MFMvanderholst <117632371+MFMvanderholst@users.noreply.github.com>
2023-05-30 11:57:39 +0200

    query gemaakt

--- app/models/RijexamenModel.php
+++ app/models/RijexamenModel.php
@@ -9,8 +9,15 @@
             $this->db = new Database();
         }
 
-        public function getOverzicht()
+        public function getOverzicht($Id)
         {
+            $sql = "SELECT exnato.Voornaam, exnato.Tussenvoegsel, exnato.Achternaam, exa.Datum, exa.Rijbewijscategorie, exa.Rijschool, exa.Stad, exa.Uitslag
+                    FROM ExamenPerExaminator epe
+                    INNER JOIN Examen exa ON epe.ExamenId = exa.Id
+                    INNER JOIN Examinator exnato ON epe.ExaminatorId = exnato.Id 
+                    WHERE ExaminatorId = $Id";
 
+            $this->db->query($sql);
+            return $this->db->resultSet();
         }
     }
