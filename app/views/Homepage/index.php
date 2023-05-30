056e41ee4cb8736242ee70de169e3362bd9db3ee
MFMvanderholst <117632371+MFMvanderholst@users.noreply.github.com>
2023-05-30 12:17:47 +0200

    nog steeds bezig met de error

--- app/views/Homepage/index.php
+++ app/views/Homepage/index.php
@@ -1,3 +1,2 @@
 <h3><?= $data['title']; ?></h3>
-<a href="http://www.mvc-2209a-framework-periode4.com/Instructeur/overzichtinstructeur">Instructeuren in dienst</a>
-<br><a href="http://www.be-p4-toets-336159/Rijexamen/Overzichtexamens">Overzicht Afgenomen Examens Examinatoren</a>
+<a href="http://www.be-p4-toets-336159/Rijexamen/Overzichtexamens">Overzicht Afgenomen Examens Examinatoren</a>
